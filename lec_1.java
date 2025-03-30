2fd31539ea1af920f2130c387ad9dd7f2fa35159
mohit kumar singh <159545677+mohitsingh841231@users.noreply.github.com>
2025-03-30 23:52:23 +0530

    Learning

---
+++ lec_1.java
@@ -0,0 +1,30 @@
+public  class lec_1{
+
+    public static void main(String[] args) {
+        int n =2;
+        switch (n) {
+           case 1:
+           System.out.println("sunday");
+           break;
+           case 2:
+           System.out.println("Monday");
+           break;
+           case 3:
+           System.out.println("tuesday");
+           break;
+           case 4:
+           System.out.println("wednesday");
+           break;
+           case 5:
+           System.out.println("thursday");
+           break;
+           case 6:
+           System.out.println("friday");
+           break;
+           case 7:
+           System.out.println("saturday");
+           
+
+        }
+    }
+}
