079bda604563ba7726e95f2c9e29f027e5e41f94
mohit kumar singh <159545677+mohitsingh841231@users.noreply.github.com>
2025-04-21 11:34:52 +0530

    practics

--- stringPrc2.java
+++ stringPrc2.java
@@ -1,19 +1,19 @@
 public class stringPrc2 {
    public static void main(String[] args) {
-        String str = "abcd";
-        for(int i =0;i<=3;i++){
-            for(int j=i+1;j<=4;j++){
-                System.out.print(str.substring(i,j)+"  ");
-                System.out.println();
-                
-                
-            }
-        }
-                String s1 = "hello";
+       
+            String s1 = "hello";
             String s3 = new String("hello");
             String s4 = new String("hello");
             String s5 = new String("hello");
             // System.out.println(s3 == s4==s5); 
+            StringBuilder sc = new StringBuilder("hello");
+            System.out.println(sc);
+            sc.setCharAt(0, 'p');
+            System.out.println(sc);
+            sc.append(10 );
+            System.out.println(sc);
+            sc.reverse();
+            System.out.println(sc);
 
    }
     
