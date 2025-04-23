fefe577a7b720ac8bae6efef2754ffba191d8a57
SumitSingh6010 <159545677+mohitsingh841231@users.noreply.github.com>
2025-04-23 23:00:12 +0530

    .

--- math.java
+++ math.java
@@ -5,7 +5,10 @@ public class math {
         // System.out.println(Math.min(a, b));
         // System.out.println(Math.sqrt(a));
         // System.out.println((int)Math.pow(a,b));
-       System.out.println( rdm(12,4));
+    //    System.out.println( rdm(12,4));
+    System.out.println(Math.round(5.9));
+    System.out.println(Math.abs(-3));
+    System.out.println(Math.ceil(5.3));
     }
     static int rdm(int a,int b){
         // return (int)(Math.random()*6)+1;
