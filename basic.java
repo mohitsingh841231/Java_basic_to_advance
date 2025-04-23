bb85d5b3d2a87b6ab8c7d97c9aae0ffad8c78435
SumitSingh6010 <159545677+mohitsingh841231@users.noreply.github.com>
2025-04-23 19:24:55 +0530

    basic practics of java

---
+++ basic.java
@@ -0,0 +1,25 @@
+public class basic {
+    public static void main(String[] args) {
+//        char abs = 'a';
+//        System.out.println(abs);
+//        int a =9;
+//        long b = a;
+//        int c= (int)b;
+//        float d = 12;
+//        int e =(int)d;
+//        int age = 129;
+//        byte newage = (byte) age;
+//
+
+        int a =12;
+        a++;
+
+
+//        System.out.println(a);
+       int c =  add(12,63);
+        System.out.println(c);
+    }
+    static  int add(int a ,int b){
+        return a>b?a:b;
+    }
+}
