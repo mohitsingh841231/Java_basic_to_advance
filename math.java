df1f3aba5b749cee18d84c1d904feda3639c7ee0
SumitSingh6010 <159545677+mohitsingh841231@users.noreply.github.com>
2025-04-23 22:05:42 +0530

    practics

---
+++ math.java
@@ -0,0 +1,14 @@
+public class math {
+    public static void main(String[] args) {
+        int a =12;
+        int b =13;
+        // System.out.println(Math.min(a, b));
+        // System.out.println(Math.sqrt(a));
+        // System.out.println((int)Math.pow(a,b));
+       System.out.println( rdm(12,4));
+    }
+    static int rdm(int a,int b){
+        // return (int)(Math.random()*6)+1;
+        return (int)(Math.random()*(b-a+1)+a);
+    }
+}
