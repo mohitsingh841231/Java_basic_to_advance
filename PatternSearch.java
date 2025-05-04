d7faf0183fac8f2ee58668a33fc42465c548d253
SumitSingh6010 <159545677+mohitsingh841231@users.noreply.github.com>
2025-05-04 14:07:41 +0530

    Update PatternSearch.java

--- PatternSearch.java
+++ PatternSearch.java
@@ -4,18 +4,18 @@ public class PatternSearch {
         int M = pat.length();
         int N = txt.length();
 
-        // A loop to slide pat[] one by one
+       
         for (int i = 0; i <= N - M; i++) {
             int j;
 
-            // For current index i, check for pattern match
+           
             for (j = 0; j < M; j++) {
                 if (txt.charAt(i + j) != pat.charAt(j)) {
                     break;
                 }
             }
 
-            // If pattern matches at index i
+          
             if (j == M) {
                 System.out.println("Pattern found at index " + i);
             }
