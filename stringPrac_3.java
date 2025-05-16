c030a3d810899dfe11f328d0d63e424053fbd6ad
SumitSingh6010 <159545677+mohitsingh841231@users.noreply.github.com>
2025-05-16 13:55:05 +0530

    practics

---
+++ stringPrac_3.java
@@ -0,0 +1,7 @@
+public class stringPrac_3 {
+    public static void main(String[] args) {
+        String str = "physics wallah";
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+    }
+}
